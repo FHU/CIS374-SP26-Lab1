f0d9fb69d03f3b38956f6e914b5ad7f3aa0ae416
Kenan Casey <kenancasey@gmail.com>
2026-02-03 11:20:35 -0600

    Add BSTNode and Begin BST

--- Lab1/BinarySearchTree.cs
+++ Lab1/BinarySearchTree.cs
@@ -1,1 +1,138 @@
-﻿
+﻿namespace Lab1;
+
+public class BinarySearchTreeNode<T> : IBinarySearchTree<T>
+{
+
+    private BinarySearchTreeNode<T>? Root {get;set;}
+
+    public bool IsEmpty => Root == null;
+
+    public int Count {get; private set;}
+
+    public int Height => throw new NotImplementedException();
+
+
+    public void Add(int key, T value)
+    {
+        if(IsEmpty)
+        {
+            Root = new BinarySearchTreeNode<T>(key, value);
+            Count++;
+        }
+        else
+        {
+            AddRecursive(key, value, Root);
+        }
+    }
+
+    private void AddRecursive(int key, T value, BinarySearchTreeNode<T> parent)
+    {
+        // existing key => update the value
+        if( key == parent.Key)
+        {
+            parent.Value = value;
+            return;
+        }
+        
+        if( key < parent.Key)
+        {
+            // look left
+            if (parent.Left == null)
+            {
+                var newNode = new BinarySearchTreeNode<T>(key, value);
+                parent.Left = newNode;
+                newNode.Parent = parent;
+                Count++;
+            }
+            else
+            {
+                AddRecursive(key, value, parent.Left);
+            }
+        } 
+        else
+        {
+            // look right 
+            if( parent.Right == null )
+            {
+                var newNode = new BinarySearchTreeNode<T>(key, value);
+                parent.Right = newNode;
+                newNode.Parent = parent;
+                Count++;
+            }
+            else
+            {
+                AddRecursive(key,value, parent.Right);
+            }   
+        }
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Search(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(int key, T value)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    // Advanced 
+
+     public BinarySearchTreeNode<T> MaxNode(BinarySearchTreeNode<T> node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BinarySearchTreeNode<T> MinNode(BinarySearchTreeNode<T> node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BinarySearchTreeNode<T> Next(BinarySearchTreeNode<T> node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BinarySearchTreeNode<T> Prev(BinarySearchTreeNode<T> node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<BinarySearchTreeNode<T>> RangeSearch(int min, int max)
+    {
+        throw new NotImplementedException();
+    }
+
+     public int? MinKey => throw new NotImplementedException();
+
+    public int? MaxKey => throw new NotImplementedException();
+
+    public Tuple<int, T>? Min => throw new NotImplementedException();
+
+    public Tuple<int, T>? Max => throw new NotImplementedException();
+
+    public double MedianKey => throw new NotImplementedException();
+
+    public List<int> InOrderKeys => throw new NotImplementedException();
+
+    public List<int> PreOrderKeys => throw new NotImplementedException();
+
+    public List<int> PostOrderKeys => throw new NotImplementedException();
+
+}
--- Lab1/BinarySearchTreeNode.cs
+++ Lab1/BinarySearchTreeNode.cs
@@ -2,7 +2,24 @@
 
 public class BinarySearchTreeNode<T>
 {
-	
+	public int Key {get;set;}
+	public T Value {get;set;}
+
+	public BinarySearchTreeNode<T>? Left {get;set;}
+	public BinarySearchTreeNode<T>? Right {get;set;}
+	public BinarySearchTreeNode<T>? Parent {get;set;}
+
+	public BinarySearchTreeNode(int key, T value)
+	{
+		Key = key;
+		Value = value;
+		Left = Right = Parent = null;
+	}
+
+    public override string ToString()
+    {
+        return $"{Key}: {Value}";
+    }
 
 }
 
